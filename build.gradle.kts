b4bf9eb3935ff13c449700b0bd918cd2e6729f15
Jörn Domnik <joern.domnik@baqend.com>
2023-02-08 12:00:16 +0100

    fix: use correct url

--- build.gradle.kts
+++ build.gradle.kts
@@ -93,7 +93,7 @@ publishing {
 
         maven {
             name = "Gitlab"
-            url = uri("https://gitlab.orestes.info/api/v4/groups/325/packages/maven")
+            url = uri("${System.getenv("CI_API_V4_URL")}/projects/151/packages/maven")
             credentials(HttpHeaderCredentials::class) {
                 name = "Job-Token"
                 value = System.getenv("CI_JOB_TOKEN")
